67499de8256472fbab17ebec5ddb1dc5ebbfe092
Jean-Marc Beaune <jm.beaune@gmail.com>
2016-11-07 21:54:21 +0100

    continue class browser and add remember me login function

--- src/AppBundle/Controller/ClassbrowserController.php
+++ src/AppBundle/Controller/ClassbrowserController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\VSCPClass;
 
 class ClassbrowserController extends Controller
 {
@@ -22,6 +23,32 @@ class ClassbrowserController extends Controller
         return $this->render('vscpclassbrowser/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
             'vscpclass' => $vscpclass,
+        ]);
+    }
+
+    /**
+     * @Route("/indexbrowser/{id}", name="vscpmaint_indexbrowser")
+     */
+    public function indexbrowserAction(Request $request, VSCPClass $selectedclass)
+    {
+
+    $vscpclass = $this->getDoctrine()
+                     ->getManager()
+                     ->getRepository('AppBundle:VSCPClass')
+                     ->getVSCPClass();
+
+    $vscpclassid = $selectedclass->getId();
+
+    $vscptype = $this->getDoctrine()
+                     ->getManager()
+                     ->getRepository('AppBundle:VSCPType')
+                     ->GetVSCPTypeByClass($vscpclassid);
+
+        return $this->render('vscpclassbrowser/typelist.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'vscpclass' => $vscpclass,
+            'selectedclass' => $selectedclass,
+            'vscptype' => $vscptype,
         ]);
     }
 
