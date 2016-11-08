2126b61b851403dbe6bec591f3cd2cabe6627f15
Jean-Marc Beaune <jm.beaune@gmail.com>
2016-11-08 21:43:14 +0100

    creating type view in class browser

--- src/AppBundle/Controller/ClassbrowserController.php
+++ src/AppBundle/Controller/ClassbrowserController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\VSCPClass;
+use AppBundle\Entity\VSCPType;
 
 class ClassbrowserController extends Controller
 {
@@ -49,6 +50,38 @@ class ClassbrowserController extends Controller
             'vscpclass' => $vscpclass,
             'selectedclass' => $selectedclass,
             'vscptype' => $vscptype,
+        ]);
+    }
+
+    /**
+     * @Route("/typedetail/{id}", name="vscpmaint_typedetail")
+     */
+    public function typedetailAction(Request $request, VSCPType $selectedtype)
+    {
+
+    $vscpclass = $this->getDoctrine()
+                     ->getManager()
+                     ->getRepository('AppBundle:VSCPClass')
+                     ->getVSCPClass();
+
+    $vscptypeid = $selectedtype->getId();
+
+    $vscpbyte = $this->getDoctrine()
+                     ->getManager()
+                     ->getRepository('AppBundle:VSCPByte')
+                     ->GetVSCPByteByType($vscptypeid);
+
+    $vscpunit = $this->getDoctrine()
+                     ->getManager()
+                     ->getRepository('AppBundle:VSCPUnit')
+                     ->GetVSCPUnitByType($vscptypeid);
+
+        return $this->render('vscpclassbrowser/typedetail.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'vscpclass' => $vscpclass,
+            'selectedtype' => $selectedtype,
+            'vscpbyte' => $vscpbyte,
+            'vscpunit' => $vscpunit,
         ]);
     }
 
